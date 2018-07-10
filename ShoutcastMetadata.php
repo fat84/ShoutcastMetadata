d96e45fbc9e2ba348bc0c91650704f305e27b311
Yurii Orlyk <aigletter@gmail.com>
2018-07-10 16:11:38 +0300

    add ShoutcastMetadata class

---
+++ ShoutcastMetadata.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * Class ShoutcastMetadata
+ */
+class ShoutcastMetadata {
+
+    const MAX_CHUNK_SIZE = 8192;
+
+    /**
+     * @var string
+     */
+    public $hostname;
+
+    /**
+     * @var int
+     */
+    public $port;
+
+    /**
+     * @var int
+     */
+    public $timeout = 30;
+
+    /**
+     * @var int
+     */
+    public $chunkSize = 128;
+
+    /**
+     * StreamMetaData constructor.
+     *
+     * @param $hostname
+     * @param int $port
+     */
+    public function __construct($hostname, $port=80) {
+        $this->hostname = $hostname;
+        $this->port = $port;
+    }
+
+    /**
+     * @return string
+     */
+    public function readStreamTitle($default='') {
+        $fp = fsockopen($this->hostname, $this->port, $errno, $errstr, $this->timeout);
+        if (!$fp) {
+            echo "$errstr ($errno)<br />\n";
+        } else {
+            $out = "GET /radio HTTP/1.1\r\n";
+            $out .= "Host: " . $this->hostname . "\r\n";
+            $out .= "Icy-MetaData:1\r\n";
+            $out .= "Connection: Close\r\n\r\n";
+            fwrite($fp, $out);
+            $resultHeaders = '';
+            while (!feof($fp)) {
+                $resultHeaders .= fgets($fp, $this->chunkSize);
+                $resArr = $this->_parseStringData("\r\n", ':', $resultHeaders);
+                if (array_key_exists('icy-metaint', $resArr)) {
+                    break;
+                }
+            }
+
+            fgets($fp);
+
+            $count = intdiv($resArr['icy-metaint'], $this->chunkSize);
+            $last = $resArr['icy-metaint'] % $this->chunkSize;
+            for ($i = 0; $i < $count; $i++) {
+                fread($fp, $this->chunkSize);
+            }
+            fread($fp, $last);
+
+            $byte = fread($fp, 1);
+            $byte = ord($byte);
+            $byte *= 16;
+
+            $metaString = fread($fp, $byte);
+            $metadata = $this->_parseStringData(';', '=', $metaString);
+
+            fclose($fp);
+        }
+
+        return isset($metadata['StreamTitle']) ? $metadata['StreamTitle'] : $default;
+    }
+
+    /**
+     * @param $delimiter1
+     * @param $delimiter2
+     * @param $string
+     *
+     * @return array
+     */
+    protected function _parseStringData($delimiter1, $delimiter2, $string) {
+        $result = [];
+        $arr = explode($delimiter1, $string);
+        foreach ($arr as $key=>$value) {
+            if (empty($value)) continue;
+            $item = explode($delimiter2, $value);
+            if (count($item) == 2) {
+                $result[$item[0]] = trim($item[1]);
+            }
+        }
+        return $result;
+    }
+}
+
+//$instance = new ShoutcastMetadata('example.com', 8020);
+//echo $instance->readStreamTitle("Unknown");
+
